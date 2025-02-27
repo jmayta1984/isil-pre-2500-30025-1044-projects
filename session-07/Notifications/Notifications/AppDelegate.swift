1c3951c26faf9bb1c80ed0bb38653470d20b6113
jmayta1984 <jmayta@pucp.edu.pe>
2025-02-26 22:13:34 -0500

    chore: initial project setup

---
+++ session-07/Notifications/Notifications/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Notifications
+//
+//  Created by user272495 on 2/26/25.
+//
+
+import UIKit
+
+
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound])
+    }
+}
--- session-07/Notifications/Notifications/ContentView.swift
+++ session-07/Notifications/Notifications/ContentView.swift
@@ -10,12 +10,81 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button ("Request permission") {
+                requestPermission()
+            }
+            
+            Button ("Calendar notification") {
+                calendarNotification()
+            }
+            
+            Button ("Schedule notification") {
+                scheduleNotification()
+            }
         }
         .padding()
+    }
+    
+    func requestPermission() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            } else {
+                print("Permission granted: \(granted)")
+            }
+        }
+    }
+    
+    func calendarNotification () {
+        let content = UNMutableNotificationContent()
+        content.title = "Calendar notification"
+        content.body = "A new notification"
+        content.sound = .default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 22
+        dateComponents.minute = 06
+        dateComponents.second = 0
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            } else {
+                print("Notification OK")
+            }
+            
+            
+        }
+    }
+    
+    func scheduleNotification () {
+        let content = UNMutableNotificationContent()
+        content.title = "Schedule notification"
+        content.body = "A new notification"
+        content.sound = .default
+        
+
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            } else {
+                print("Notification OK")
+            }
+            
+            
+        }
     }
 }
 
