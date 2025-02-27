52f46a797bfa8d3fb255cdf2bfe4928c11eae884
jmayta1984 <jmayta@pucp.edu.pe>
2025-02-26 22:12:27 -0500

    chore: initial project setup

---
+++ session-07/Notifications/Notifications/NotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationsApp.swift
+//  Notifications
+//
+//  Created by user272495 on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct NotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
